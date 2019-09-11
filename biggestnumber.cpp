c45d1aa445112e4278484846d66501e728890e5f
cnmullins <cnmullins@mail.csuchico.edu>
2019-09-11 16:12:34 -0700

    This version stores whole numbers the user inputs.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,18 +1,22 @@
-//Author:
+//Author: Christian Mullins
 #include<iostream>
 
 int main()
 {
   //declare variables here
+  int numberOne = 0;
+  int numberTwo = 0;
+  int biggestNumber = 0;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
+  std::cin>>numberOne;
 
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
+  std::cin>>numberTwo;
 
   //determine if first or second is bigger and store that in a third variable
-
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
